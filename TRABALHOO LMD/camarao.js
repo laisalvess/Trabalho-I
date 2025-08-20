b7a84dda77cb89d4d08ee22b6289b4c958777fc5
laisalvess <alves.12lais@gmail.com>
2025-08-20 17:16:52 -0300

    Add files via upload

---
+++ TRABALHOO LMD/camarao.js
@@ -0,0 +1,19 @@
+function verificar(){
+const usuario= document.getElementById("login").value;
+const senha= document.getElementById("senha").value;
+const título = document.getElementById("título");
+const usuarios =[
+    {login:"Maria Alice",senha:"1234" },
+    {login:"Daniel", senha:"567"},
+    {login:"Laís", senha:"890"}];
+    let encontrado = false;
+    for(i=0; i< usuarios.length; i++){
+        if(usuario == usuarios[i].login && senha == usuarios[i].senha){
+            encontrado =true;
+            título.innerHTML="Seja bem-vindo a página "  + usuarios[i].login + "!";
+        }
+    }
+    if(!encontrado){
+        título.innerHTML="Erro usuário não encontrado =( ";
+    }
+}
