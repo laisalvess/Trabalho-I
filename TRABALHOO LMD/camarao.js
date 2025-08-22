17a9cb8670c84ef72c7dab6c7664975c43e62c8b
laisalvess <alves.12lais@gmail.com>
2025-08-22 17:08:42 -0300

    Update camarao.js

--- TRABALHOO LMD/camarao.js
+++ TRABALHOO LMD/camarao.js
@@ -11,9 +11,20 @@ const usuarios =[
         if(usuario == usuarios[i].login && senha == usuarios[i].senha){
             encontrado =true;
             título.innerHTML="Seja bem-vindo a página "  + usuarios[i].login + "!";
+window.location.href="https://prozeducacao.com.br/"
+
+            
         }
     }
     if(!encontrado){
-        título.innerHTML="Erro usuário não encontrado =( ";
+    título.innerHTML="Erro usuário não encontrado =( ";
     }
+
+
+
+setTimeout(() => {
+    título.innerHTML="Login SC";
+},5000);
+document.getElementById("login").value = "";
+document.getElementById("senha").value = "";
 }
